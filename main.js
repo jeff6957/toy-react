d0efab40a9d1eda22d2a883e98bf2698767494e5
王志飞 <695766301@qq.com>
2020-08-17 10:11:54 +0800

    初始化

---
+++ main.js
@@ -0,0 +1,36 @@
+
+import { createElement, Component, render } from './toy-react';
+
+// MyComponnent 是一个类  如何渲染成dom
+
+// MyComponnent render 部分jsx 语法转换后
+
+// createElement("div", null, createElement("h1", null, "my Component"), this.children)
+// !! 这里 createElement 出来的就是一个节点了, 被 父类 Component 的 get root 内部调用了 render 获得了对应dom
+
+class MyComponnent extends Component {
+  render () {
+    return <div>
+      <h1>my Component</h1>
+      {this.children}
+    </div>
+  }
+}
+
+
+// createElement(MyComponnent, {
+//   id: 'a',
+//   "class": "c",
+// }, createElement("div", null, "abc"), createElement("div", null), createElement("div", null))
+
+render(<MyComponnent id="a" class="b">
+  <div>sfasdf</div>
+  <div></div>
+  <div></div>
+</MyComponnent>, document.body)
+
+/** dd
+ * 
+ */
+
+
---
+++ toy-react.js
@@ -0,0 +1,78 @@
+
+
+class ElementWrapper {
+  constructor(type) {
+    this.root = document.createElement(type);
+  }
+  setAttribute(name, value) {
+    this.root.setAttribute(name, value)
+  }
+  appendChild(component) {
+    this.root.appendChild(component.root)
+  }
+}
+
+class TextNodeWrapper {
+  constructor(content) {
+    this.root = document.createTextNode(content);
+  }
+}
+
+export class Component {
+  constructor() {
+    this.props = Object.create(null);
+    this.children = [];
+    this._root = null;
+  }
+  setAttribute(name, value) {
+    this.props[name] = value;
+  }
+  appendChild(component) {
+    this.children.push(component);
+  }
+  get root () {
+    if (!this._root) {
+      this._root = this.render().root;
+    }
+    return this._root;
+  }
+}
+
+
+export function createElement(type, attributes, ...children) {
+  
+  let e;
+  if (typeof type == 'string') {
+    // 为什么要这样写, 包装一层 统一返回
+    e = new ElementWrapper(type);
+  } else {
+    // !! 如果传进来的是一个类 实例化 得到对象
+    e = new type;
+  }
+
+  for (let p in attributes) {
+    e.setAttribute(p, attributes[p]);
+  }
+  let insertChildren = (children) => {
+    for (let child of children) {
+      if (typeof child == 'string') {
+        child = new TextNodeWrapper(child);
+      }
+      if ((typeof child === 'object') && (child instanceof Array)) {
+        insertChildren(child)
+      } else {
+        // 这里添加的 也是 通过createElement 执行得到的返回值
+        e.appendChild(child);
+      }
+    }
+  }
+
+  insertChildren(children)
+
+  return e;
+}
+
+
+export function render (component, parentElement) {
+  parentElement.appendChild(component.root)
+}
